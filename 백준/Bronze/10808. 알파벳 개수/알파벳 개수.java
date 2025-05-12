bf9db148011e451b09a78382d8975990618e02fd
Lee Won Jin <127071704+zldzldzz@users.noreply.github.com>
2025-05-12 13:27:01 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 108 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,20 +1,22 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int [] k=new int[26];
-        String str = scanner.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str= br.readLine();
+        int[] answer= new int[26];
         for (int i = 0; i < str.length(); i++) {
-            char c= str.charAt(i);
-            if('a' <= c && c <= 'z'){
-                k[c-'a']++;
-
+            if(str.charAt(i)>='a'&&str.charAt(i)<='z'){
+                answer[str.charAt(i)-'a']++;
             }
         }
-        for (int i : k) {
-            System.out.print(i+" ");
+        for (int i : answer) {
+            bw.write(String.valueOf(i)+" ");
+            bw.flush();
         }
+        bw.close();
+        br.close();
 
     }
 }
