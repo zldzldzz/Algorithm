a5a209dddba41b9fff65209252e17c7bf72a3714
Lee Won Jin <127071704+zldzldzz@users.noreply.github.com>
2025-06-18 14:00:09 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw.write("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+        bw.flush();
+        bw.close();
+    }
+}
