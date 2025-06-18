e5c6837bda33775d612da7df17d3e729cc1a18bf
Lee Won Jin <127071704+zldzldzz@users.noreply.github.com>
2025-06-18 13:58:50 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw.write("\\    /\\ \n )  ( ')\n(  /  )\n \\(__)|");
+        bw.flush();
+        bw.close();
+    }
+}
