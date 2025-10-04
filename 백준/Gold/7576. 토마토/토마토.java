ecbe59695f904b57c53a5074dc15bd018b1f9410
LeeWonJin <127071704+zldzldzz@users.noreply.github.com>
2025-10-05 01:08:20 +0900

    [Gold V] Title: 토마토, Time: 672 ms, Memory: 141104 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,66 +1,63 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[][] map = new String[m][n];
-        boolean[][] visited = new boolean[m][n];
+		int result = 0;
 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] map = new int[m][n];
+		boolean[][] visited = new boolean[m][n];
 
-        int[] dx={1, 0, -1, 0};
-        int[] dy={0,-1, 0, 1};
+		ArrayDeque<Integer[]> q = new ArrayDeque<>();
+		int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-        for (int i = 0; i < m; i++) {
-            map[i]=br.readLine().split(" ");
-        }
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 1) {
+					q.add(new Integer[] {i, j, 0});
+					visited[i][j] = true;
+				}
+			}
+		}
+		while (!q.isEmpty()) {
+			Integer[] now = q.poll();
+			result = Math.max(result, now[2]);
+			for (int i = 0; i < 4; i++) {
+				int x = now[0] + dir[i][0];
+				int y = now[1] + dir[i][1];
+				if (x >= 0 && y >= 0 && x < m && y < n && !visited[x][y]) {
+					if (map[x][y] == 0) {
+						visited[x][y] = true;
+						q.add(new Integer[] {x, y, now[2] + 1});
+					}
+				}
+			}
+		}
 
-        Queue<Integer[]> q = new LinkedList<>();
-        int max=0;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (map[i][j] == 0 && !visited[i][j]) {
+					result = -1;
+				}
+			}
+		}
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(map[i][j].equals("1")&&!visited[i][j]){
-                    q.add(new Integer[]{i,j, 0});
-                    visited[i][j]=true;
-                }
-
-            }
-        }
-
-        while (!q.isEmpty()){
-            Integer[] now = q.poll();
-            max=Math.max(max,now[2]);
-            for (int k = 0; k < 4; k++) {
-                int x = now[0]+dx[k];
-                int y = now[1]+dy[k];
-                if(x>=0&&y>=0&&x<m&&y<n&&!visited[x][y]){
-                    if(map[x][y].equals("0")){
-                        q.add(new Integer[]{x,y,now[2]+1});
-                        visited[x][y]=true;
-                    }
-                }
-            }
-        }
-
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(visited[i][j]==false&&map[i][j].equals("0")){
-                    max=-1;
-                }
-            }
-        }
-
-        bw.write(String.valueOf(max));
-        bw.flush();
-        bw.close();
-        br.close();
-
-    }
+		bw.write(String.valueOf(result));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
