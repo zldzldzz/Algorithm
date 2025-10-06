479f05bf4b8a7fc52b6280d5aa24061ea21bfcb2
LeeWonJin <127071704+zldzldzz@users.noreply.github.com>
2025-10-07 04:45:53 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n, m;
+	static int[] arr;
+	static boolean[] visited;
+
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	static void dfs(int length, int start) throws IOException {
+
+		if (length == m) {
+			for (int i = 0; i < length; i++) {
+				bw.write(String.valueOf(arr[i]) + " ");
+			}
+			bw.newLine();
+			return;
+		}
+
+		for (int i = start; i <= n; i++) {
+			if (!visited[i]) {
+
+				arr[length] = i;
+				visited[i] = true;
+				dfs(length + 1, i + 1);
+				visited[i] = false;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[m];
+		visited = new boolean[n + 1];
+		dfs(0, 1);
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
